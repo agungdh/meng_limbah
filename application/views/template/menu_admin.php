6a20e35bd274a9ef706c6cab9e8309b0a0d94c29
agungdh <agungdh@live.com>
2018-03-04 21:19:30 +0700

    update menu

--- application/views/template/menu_admin.php
+++ application/views/template/menu_admin.php
@@ -1,9 +1,30 @@
 <li class="header">MENU UTAMA</li>
 
 <li>
-	<a href="<?php echo base_url("fakultas"); ?>">
+	<a href="<?php echo base_url("golongan"); ?>">
 		<i class="fa fa-users text-aqua"></i>
-		<span>Data Fakultas</span>
+		<span>Data Golongan</span>
+	</a>
+</li>
+
+<li>
+	<a href="<?php echo base_url("jenis"); ?>">
+		<i class="fa fa-users text-aqua"></i>
+		<span>Data Jenis</span>
+	</a>
+</li>
+
+<li>
+	<a href="<?php echo base_url("limbah"); ?>">
+		<i class="fa fa-users text-aqua"></i>
+		<span>Data Limbah</span>
+	</a>
+</li>
+
+<li>
+	<a href="<?php echo base_url("sumber"); ?>">
+		<i class="fa fa-users text-aqua"></i>
+		<span>Data Sumber</span>
 	</a>
 </li>
 
@@ -11,19 +32,5 @@
 	<a href="<?php echo base_url("user"); ?>">
 		<i class="fa fa-users text-aqua"></i>
 		<span>Data User</span>
-	</a>
-</li>
-
-<li>
-	<a href="<?php echo base_url("versi"); ?>">
-		<i class="fa fa-users text-aqua"></i>
-		<span>Data Borang</span>
-	</a>
-</li>
-
-<li>
-	<a href="<?php echo base_url("penilaian"); ?>">
-		<i class="fa fa-users text-aqua"></i>
-		<span>Data Penilaian</span>
 	</a>
 </li>
