f8818c847f61917b42cda482a289ad0ebd11c9ce
agungdh <agungdh@live.com>
2018-03-04 17:11:14 +0700

    update

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -18,20 +18,15 @@ class Test extends CI_Controller {
 		<table border="1">
 			<thead>
 				<tr>
-			    <th rowspan="2">No</th>
-			    <th rowspan="2">Limbah</th>
-			    <th rowspan="2">Tanggal Masuk</th>
-			    <th rowspan="2">Sumber</th>
-			    <th colspan="2">Jumlah</th>
-			  </tr>
-			  <tr>
-			    <th>Buah / Drum</th>
-			    <th>KG</th>
+			    <th>No</th>
+			    <th>Limbah</th>
+			    <th>Tanggal Masuk</th>
+			    <th>Sumber</th>
+			    <th>Jumlah (KG)</th>
 			  </tr>
 			</thead>
 			<tbody>
 			<?php 
-			$buah = 0;
 			$jumlah = 0;
 			foreach ($this->m_test->ambil_parent_limbah() as $item) {
 				?>
@@ -40,7 +35,6 @@ class Test extends CI_Controller {
 			    </tr>
 			    <?php
 			    foreach ($this->m_test->ambil_child_limbah($item->id_limbah) as $item2) {
-			    	$buah += $item2->buah;
 			    	$jumlah += $item2->jumlah;
 			    	$item_jumlah = explode('.', $item2->jumlah);
 			    	if ($item_jumlah[1] == 0) {
@@ -54,7 +48,6 @@ class Test extends CI_Controller {
 				    	<td><?php echo $item2->sub_limbah; ?></td>
 				    	<td><?php echo $this->pustaka->tanggal_indo_string($item2->tanggal); ?></td>
 				    	<td><?php echo $item2->sumber; ?></td>
-				    	<td><?php echo $item2->buah; ?></td>
 				    	<td><?php echo $item_jumlah; ?></td>
 			    	</tr>
 			    	<?php
@@ -62,11 +55,9 @@ class Test extends CI_Controller {
 			    ?>
 			    <tr>
 				    <td colspan="4">Total</td>
-				    <td><?php echo $buah; ?></td>
 				    <td><?php echo $jumlah; ?></td>
 			    </tr>
 				<?php
-				$buah = 0;
 				$jumlah = 0;
 			}
 			?>
