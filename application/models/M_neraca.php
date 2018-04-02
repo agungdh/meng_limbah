343a3e21831cb648a67662003326a463b13a51c7
agungdh <agungdh@live.com>
2018-04-02 16:01:21 +0700

    update

--- application/models/M_neraca.php
+++ application/models/M_neraca.php
@@ -114,7 +114,7 @@ class M_neraca extends CI_Model{
 		return $row;
 	}
 
-	function ambil_jumlah_masuk_dari_limbah_lalu($id_unit, $id_limbah, $bulan_awal, $bulan_akhir, $tahun){
+	function ambil_jumlah_masuk_dari_limbah_lalu($id_unit, $id_limbah, $bulan_awal, $tahun){
 		$sql = "SELECT sum(jumlah) total
 				FROM masuk m, sub_limbah sl, limbah l, sumber s
 				WHERE m.id_unit = ?
@@ -122,50 +122,50 @@ class M_neraca extends CI_Model{
 				AND m.id_sub_limbah = sl.id
 				AND sl.id_limbah = l.id
 				AND m.id_sumber = s.id
-				AND month(tanggal) BETWEEN ? AND ?
+				AND month(tanggal) < ?
 				AND year(tanggal) = ?
 				";
-		$query = $this->db->query($sql, array($id_unit, $id_limbah, $bulan_awal, $bulan_akhir, $tahun));
+		$query = $this->db->query($sql, array($id_unit, $id_limbah, $bulan_awal, $tahun));
 		$row = $query->row();
 		return $row;
 	}
 
-	function ambil_jumlah_masuk_dari_limbah_lalu_semuasemua($id_limbah, $bulan_awal, $bulan_akhir, $tahun){
+	function ambil_jumlah_masuk_dari_limbah_lalu_semuasemua($id_limbah, $bulan_awal, $tahun){
 		$sql = "SELECT sum(jumlah) total
 				FROM masuk m, sub_limbah sl, limbah l, sumber s
 				WHERE l.id = ?
 				AND m.id_sub_limbah = sl.id
 				AND sl.id_limbah = l.id
 				AND m.id_sumber = s.id
-				AND month(tanggal) BETWEEN ? AND ?
+				AND month(tanggal) < ?
 				AND year(tanggal) = ?
 				";
-		$query = $this->db->query($sql, array($id_limbah, $bulan_awal, $bulan_akhir, $tahun));
+		$query = $this->db->query($sql, array($id_limbah, $bulan_awal, $tahun));
 		$row = $query->row();
 		return $row;
 	}
 
-	function ambil_jumlah_keluar_dari_limbah_lalu($id_unit, $id_limbah, $bulan_awal, $bulan_akhir, $tahun){
+	function ambil_jumlah_keluar_dari_limbah_lalu($id_unit, $id_limbah, $bulan_awal, $tahun){
 		$sql = "SELECT sum(jumlah) total
 				FROM keluar
 				WHERE id_unit = ?
 				AND id_limbah = ?
-				AND month(tanggal) BETWEEN ? AND ?
+				AND month(tanggal) < ?
 				AND year(tanggal) = ?
 				";
-		$query = $this->db->query($sql, array($id_unit, $id_limbah, $bulan_awal, $bulan_akhir, $tahun));
+		$query = $this->db->query($sql, array($id_unit, $id_limbah, $bulan_awal, $tahun));
 		$row = $query->row();
 		return $row;
 	}
 
-	function ambil_jumlah_keluar_dari_limbah_lalu_semuasemua($id_limbah, $bulan_awal, $bulan_akhir, $tahun){
+	function ambil_jumlah_keluar_dari_limbah_lalu_semuasemua($id_limbah, $bulan_awal, $tahun){
 		$sql = "SELECT sum(jumlah) total
 				FROM keluar
 				WHERE id_limbah = ?
-				AND month(tanggal) BETWEEN ? AND ?
+				AND month(tanggal) < ?
 				AND year(tanggal) = ?
 				";
-		$query = $this->db->query($sql, array($id_limbah, $bulan_awal, $bulan_akhir, $tahun));
+		$query = $this->db->query($sql, array($id_limbah, $bulan_awal, $tahun));
 		$row = $query->row();
 		return $row;
 	}
