de5e5e5f64aba8090f7bff29833acb6c06c767e6
agungdh <agungdh@live.com>
2018-04-09 10:44:38 +0700

    update

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -6,6 +6,75 @@ class Test extends CI_Controller {
 		parent::__construct();
 	}
 
+	function test1($id_unit){
+		?>
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title></title>
+		</head>
+		<body>
+
+		<table border="1">
+			<thead>
+				<tr>
+					<th rowspan="2">Limbah</th>
+					<th rowspan="2">Sisa</th>
+					<th colspan="2">Harus Dibuang</th>
+				</tr>
+				<tr>
+					<th>Hari</th>
+					<th>Jumlah</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+					foreach ($this->db->get('limbah')->result() as $item) {
+						?>
+						<tr>
+							<td><?php echo $item->limbah; ?></td>
+							<?php
+							//masuk
+							$this->db->select_sum('jumlah');
+							$where['id_unit'] = $id_unit;
+							$where['id_limbah'] = $item->id;
+							$this->db->where($where);
+							$jumlah_masuk = $this->db->get('v_masuk_id_limbah')->row()->jumlah ?: 0;
+							unset($where);
+
+							//keluar
+							$this->db->select_sum('jumlah');
+							$where['id_unit'] = $id_unit;
+							$where['id_limbah'] = $item->id;
+							$this->db->where($where);
+							$jumlah_keluar = $this->db->get('keluar')->row()->jumlah ?: 0;
+							unset($where);							
+
+							//jumlah
+							$jumlah = $jumlah_masuk - $jumlah_keluar;
+							?>
+							<td><?php echo $jumlah_masuk . " - " . $jumlah_keluar . " = " . $jumlah; ?></td>
+							<?php
+							if ($jumlah != 0) {
+								?>
+								<?php
+							} else {
+								$hari_dibuang = $jumlah_dibuang = '-';
+							}
+							?>
+							<td><?php echo $hari_dibuang; ?></td>
+							<td><?php echo $jumlah_dibuang; ?></td>
+						</tr>
+						<?php
+					}
+					?>
+			</tbody>
+		</table>
+		</body>
+		</html>
+		<?php
+	}
+
 	function index($id_unit) {
 		$where['id_unit'] = $id_unit;
 		$this->db->where($where);
