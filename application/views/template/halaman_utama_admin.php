1d4a5005ef8328caa6ccbea793d252b0c41136c5
agungdh <agungdh@live.com>
2018-04-11 11:45:14 +0700

    update

--- application/views/template/halaman_utama_admin.php
+++ application/views/template/halaman_utama_admin.php
@@ -32,7 +32,7 @@
     <div class="box-header with-border">
       <h3 class="box-title">
           <p>Jumlah (KG) Limbah Keluar</p>
-          <p>Tahun <input type="number" name="tahun_keluar" min="1900" max="2900" value="<?php echo $tahun_masuk; ?>"></p>
+          <p>Tahun <input type="number" name="tahun_keluar" min="1900" max="2900" value="<?php echo $tahun_keluar; ?>"></p>
           <p>Unit <select name="unit_keluar">
             <option <?php echo $this->input->get('unit_keluar') == null ? "Selected" : null; ?> value="">Semua Unit</option>
             <?php
@@ -53,7 +53,35 @@
     </div>
     <!-- /.box-body -->
   </div>
+  <!-- /.box -->  
+
+  <!-- BAR CHART -->
+  <div class="box box-primary">
+    <div class="box-header with-border">
+      <h3 class="box-title">
+          <p>Jumlah (KG) Limbah di TPS</p>
+          <p>Unit <select name="unit_sisa">
+            <option <?php echo $this->input->get('unit_sisa') == null ? "Selected" : null; ?> value="">Semua Unit</option>
+            <?php
+            foreach ($this->db->get('unit')->result() as $item) {
+              ?>
+              <option <?php echo $this->input->get('unit_sisa') == $item->id ? "Selected" : null; ?> value="<?php echo $item->id; ?>"><?php echo $item->unit; ?></option>
+              <?php
+            }
+            ?>
+          </select></p>
+          <input type="submit" name="">
+      </h3>
+    </div>
+    <div class="box-body">
+      <div class="chart">
+        <canvas id="myChartss" style="height:230px"></canvas>
+      </div>
+    </div>
+    <!-- /.box-body -->
+  </div>
   <!-- /.box -->
+
 </form>
 
 .
@@ -105,6 +133,7 @@ $triwulan[4] = "BETWEEN 10 AND 2";
 ?>
 <script type="text/javascript">
   $(function() {
+    //masuk
     var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
@@ -197,6 +226,8 @@ $triwulan[4] = "BETWEEN 10 AND 2";
             }
         }
     });
+
+    //keluar
     var ctx = document.getElementById("myCharts").getContext('2d');
     var myCharts = new Chart(ctx, {
         type: 'bar',
@@ -273,6 +304,57 @@ $triwulan[4] = "BETWEEN 10 AND 2";
                 }
                 ?>
                 // '12', '19', '3', '5', '2', '3', '3', '3', '3', '3'
+                ],
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+  
+    //sisa
+    var ctx = document.getElementById("myChartss").getContext('2d');
+    var myChartss = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: <?php echo $limbah; ?>,
+            datasets: [{
+                label: 'Limbah',
+                data: [
+                <?php
+                foreach ($this->db->get('limbah')->result() as $item) {
+                  $this->db->select_sum('jumlah');
+                  $this->db->where('id_limbah', $item->id);
+                  if ($this->input->get('unit_sisa') != null) {
+                    $where_sisa['id_unit'] = $this->input->get('unit_sisa');
+                    $this->db->where($where_sisa);
+                  }
+                  $masuk = $this->db->get('v_masuk_id_limbah')->row()->jumlah ?: '0';
+                  
+                  $this->db->select_sum('jumlah');
+                  $this->db->where('id_limbah', $item->id);
+                  if ($this->input->get('unit_sisa') != null) {
+                    $where_sisa['id_unit'] = $this->input->get('unit_sisa');
+                    $this->db->where($where_sisa);
+                  }
+                  $keluar = $this->db->get('keluar')->row()->jumlah ?: '0';
+                  
+                  // echo $this->db->last_query(); exit();
+                  $sisa = $masuk - $keluar;
+
+                  echo "'" . $sisa . "'" . ",";
+                }
+                ?>
+                // '1', '19', '3', '5', '2', '3', '3', '3', '3', '3'
                 ],
                 backgroundColor: 'rgba(54, 162, 235, 0.2)',
                 borderColor: 'rgba(54, 162, 235, 1)',
