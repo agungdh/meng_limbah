4e714bc9b0ab169dce6124be02032788fa3e9da0
agungdh <agungdh@live.com>
2018-04-16 15:12:36 +0700

    update

--- application/views/template/menu_admin.php
+++ application/views/template/menu_admin.php
@@ -2,6 +2,13 @@
 <li class="header">MENU UTAMA</li>
 
 <li>
+    <a href="<?php echo base_url(); ?>">
+        <i class="fa fa-dashboard text-aqua"></i>
+        <span>Dashboard</span>
+    </a>
+</li>
+
+<li>
 	<a href="#">
 		<i class="fa fa-database text-aqua"></i>
 		<span>Data Master</span>
--- application/views/template/menu_other.php
+++ application/views/template/menu_other.php
@@ -1,6 +1,13 @@
 <li class="header">MENU UTAMA</li>
 
 <li>
+	<a href="<?php echo base_url(); ?>">
+		<i class="fa fa-dashboard text-aqua"></i>
+		<span>Dashboard</span>
+	</a>
+</li>
+
+<li>
 	<a href="<?php echo base_url("masuk"); ?>">
 		<i class="fa fa-sign-in text-aqua"></i>
 		<span>Data Limbah Masuk</span>
